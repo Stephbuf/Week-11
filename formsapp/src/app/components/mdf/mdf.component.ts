511ce418f84e2415302e4e7be6082b118606f60f
Stephanie <stephaniebtot3@gmail.com>
2025-04-02 18:13:42 -0600

    forms update

--- formsapp/src/app/components/mdf/mdf.component.ts
+++ formsapp/src/app/components/mdf/mdf.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-mdf',
@@ -12,13 +12,26 @@ userForm;
 
 constructor(private fb: FormBuilder){
   this.userForm = fb.group({
-    name: [''],
+    name: ['', [Validators.required, Validators.minLength(3)]],
     email: [''],
     phone: ['']
   });
 }
 
 onSubmit(){
+  this.nameFormControl;
   console.log(this.userForm.value);
+}
+
+//Getter methods 
+
+get nameFormControl() {
+  return this.userForm.get('name')!; // will return name from control 
+}
+get emailFormControl() {
+  return this.userForm.get('email')!; // will return email from control 
+}
+get phoneFormControl() {
+  return this.userForm.get('phone')!; // will return phone from control 
 }
 }
